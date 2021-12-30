717793800ef465fc0ad68c07b3582df662609212
gerald-annan <geraldannan400@gmail.com>
2021-12-30 22:52:27 +0000

    Passed solution

--- lib/take_a_number.ex
+++ lib/take_a_number.ex
@@ -1,5 +1,27 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(fn -> machine(0) end)
+  end
+
+  defp machine(state) do
+    state =
+      receive do
+        {:report_state, pid} ->
+          send(pid, state)
+          state
+
+        {:take_a_number, pid} ->
+          new_state = state + 1
+          send(pid, new_state)
+          new_state
+
+        :stop ->
+          exit(:stop)
+
+        _ ->
+          state
+      end
+
+    machine(state)
   end
 end
